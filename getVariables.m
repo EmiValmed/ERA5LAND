20b2881cc5cf6163ff73a77a2d83e674f851378d
Emixi Valdez <60191438+EmiValmed@users.noreply.github.com>
2022-03-23 00:15:04 -0400

    Update getVariables.m

--- getVariables.m
+++ getVariables.m
@@ -1,8 +1,14 @@
-function [dataVar] = getVariables(VarName,ncid)
+function [dataVar,Date] = getVariables(VarName,ncid)
 
+% Variable time serie
 sf   = netcdf.getAtt(ncid,netcdf.inqVarID(ncid,VarName),'scale_factor');
 ao   = netcdf.getAtt(ncid,netcdf.inqVarID(ncid,VarName),'add_offset');
 dataVar = netcdf.getVar(ncid,netcdf.inqVarID(ncid,VarName),'double');
 dataVar = (dataVar .* sf + ao );
 
+% Date 
+Date = netcdf.getVar(ncid,netcdf.inqVarID(ncid,'time'),'double');
+Date = datenum(Date./24) + datenum('1900-01-01 00:00:00');
+
+end
 end
