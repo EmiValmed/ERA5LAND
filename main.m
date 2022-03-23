a1213438f1bd5b50a907fd37d7440fe9ea9e23a4
Emixi Valdez <60191438+EmiValmed@users.noreply.github.com>
2022-03-23 12:10:51 -0400

    Update main.m

--- main.m
+++ main.m
@@ -2,21 +2,23 @@ clear; close all; clc
 %% Declarations
 
 % Directories
-dataPath= 'Path where the .nc files are located'                                 ; addpath(dataPath);
-OutPath=  'Path where you want to save the results of the concatenated values'   ; addpath(OutPath);
+dataPath= 'Path where the .nc files are located'  ;                                  % To modify                          
+OutPath=  'Path where you want to save the results of the concatenated values'   ;   % To modify      
+addpath(dataPath);
+addpath(OutPath);
 
 if ~exist(fullfile(OutPath), 'dir')
     mkdir(fullfile(OutPath)); addpath(OutPath);
 end
 
-% Datebase
+% Variable specifications
 VarName     = 't2m';                   % To modify
 Name        = 'Temperature';           % To modify
-AccVariable = 0; % 1/0                 % To modify
+AccVariable = 0; % could be 0 or 1     % To modify
 
 % Time difference between UTC and the local time
-TimeZone  = 'UTC';
 LocalZone = '-05:00';  % To modify
+
                        % ----------------------------------------------------------------------------------------------------
                        % Note: You can specify the time zone value as a character vector of the form +HH:mm or -HH:mm, which
                        % represents a time zone with a fixed offset from UTC that does not observe daylight saving time.
@@ -27,7 +29,7 @@ LocalZone = '-05:00';  % To modify
 
 %% -------------------------------------------------- DO NOT TOUCH FROM HERE ------------------------------------------------
 
-%% Extracting and concatenating variables to Quebec local time
+%% Extracting and concatenating variables to local time
 
 ERA5LAND_Extract(VarName_nc,VarName,VarType,LocalZone,dataPath,OutPath)
 
