ad194716ae7306d4e47ff6018df7435017fdb7ff
Emixi Valdez <60191438+EmiValmed@users.noreply.github.com>
2022-03-23 14:47:05 -0400

    Update r2cFile.m

--- r2cFile.m
+++ r2cFile.m
@@ -1,16 +1,14 @@
-function r2cFile(Var,VarName,Unit,latitude,longitude,time)
+function r2cFile(Data,VarName,Unit,latitude,longitude,Date,OutPath)
 
 format_out =('"yyyy/mm/dd HH:MM:SS.000"');
-dtime_str  = datestr(time, format_out);
+dtime_str  = datestr(Date, format_out);
 
 % création de la première grille 7x5 et le texte 
-FileNme = sprintf('%s.r2c',VarName);
-fVar = fopen(FileNme, 'w');          % ouvrir le fichier r2c
+FileNmeAndPath = fullfile(sprintf('%s/%s.r2c',OutPath,VarName));
+fVar = fopen(FileNmeAndPath, 'w');          % ouvrir le fichier r2c
 fmt = [repmat(' %.2f ', 1, 5) '\n']; % format des données
 xOrigin = longitude(1);
 yOrigin = latitude(end);
-
-
 
 fprintf(fVar, '%s\n','########################################',':FileType r2c  ASCII  EnSim 1.0','#','# DataType               2D Rect Cell','#');
 fprintf(fVar, '%s\n',':Application             FORTRAN',':Version                 1.0.0',':WrittenBy               MSC/HAL/GIWS',':CreationDate            01/25/  13 19:12:44');
@@ -21,9 +19,9 @@ fprintf(fVar, '%s\n',':xCount                   5',':yCount                   7'
 fprintf(fVar, '%s\n', ':endHeader');
 
 
-for j = 1: size(Var,3) % numéro total du pas de temps
+for j = 1: size(Data,3) % numéro total du pas de temps
     fprintf(fVar, '%s %8d %7d %s\n', ':Frame',j ,j, dtime_str(j,:)); % ligne des dates
-    Variable = squeeze(Var(:,:,j));
+    Variable = squeeze(Data(:,:,j));
     [Xq,Yq] = meshgrid(1:5,1:7); % resize 5x7
     Variable = interp2(Variable, Xq, Yq);% interpolation of data in the resize 5x7
     fprintf(fVar, fmt, Variable.');
