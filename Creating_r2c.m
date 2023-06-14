610beba1d5430fc30424c3df79b0aa6d526fc015
Emixi Valdez <60191438+EmiValmed@users.noreply.github.com>
2023-06-14 17:34:08 -0400

    Update Creating_r2c.m
    Selecting simulation period

--- Creating_r2c.m
+++ Creating_r2c.m
@@ -1,4 +1,4 @@
-function Creating_r2c (OutPath,xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+function Creating_r2c(OutPath,xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc,DateSim)
 %% Declarations
 
 % Directories
@@ -9,26 +9,31 @@ addpath(genpath('OutputERA5-Land'));
 
 load('Precipitation.mat','Date','Data');
 tp = Data*(1000/3600); 
+[Date, tp] = util_SelectPeriod(Date,DateSim,tp);
 
-r2cFile(tp,Date,OutPath,'rain',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(tp,Date,OutPath,'rain',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
 
 %% Surface pressure in Pa
 load('Pressure.mat','Date','Data');
 sp = Data;
+[Date, sp] = util_SelectPeriod(Date,DateSim,sp);
 
-r2cFile(sp,Date,OutPath,'pres',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(sp,Date,OutPath,'pres',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
 
 %% Air surface temperature in K
 load('Temperature.mat','Date','Data');
 t2m = Data;
+[Date, t2m] = util_SelectPeriod(Date,DateSim,t2m);
 
-r2cFile(t2m,Date,OutPath,'temperature',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(t2m,Date,OutPath,'temperature',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
 
 %% Specific Humidity in Kg/Kg
 
 % Dew point temperature in K
 load('DewPoint.mat','Date','Data');
 d2m = Data;
+[Date, d2m] = util_SelectPeriod(Date,DateSim,d2m);
+
 
 epsilon = 622; % g kg-1
 LvRv = 5423; % K
@@ -45,28 +50,32 @@ e_a = RH.*(10.^(aux)); %surface presure (Pa)
 q(:,:,i) = 0.622*e_a./(sp(:,:,i) - 0.378*e_a);
 end
 
-r2cFile(q,Date,OutPath,'humidity',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(q,Date,OutPath,'humidity',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
 
 %% Shortwave  in W/m2
 load('Shortwave.mat','Date','Data');
 ssrd = Data*(1/3600);
+[Date, ssrd] = util_SelectPeriod(Date,DateSim,ssrd);
 
-r2cFile(ssrd,Date,OutPath,'shortwave',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(ssrd,Date,OutPath,'shortwave',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
 
 %% Longwave  in W/m2
 load('Longwave.mat','Date','Data');
 strd = Data*(1/3600);
+[Date, strd] = util_SelectPeriod(Date,DateSim,strd);
 
-r2cFile(strd,Date,OutPath,'longwave',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(strd,Date,OutPath,'longwave',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
 
 %% u10 Wind component
 load('Wind_u.mat','Date','Data');
 u10 = Data;
+[~, u10] = util_SelectPeriod(Date,DateSim,u10);
 
 % v10 Wind component
 load('Wind_v.mat','Date','Data');
 v10 = Data;
+[Date, v10] = util_SelectPeriod(Date,DateSim,v10);
 
 ws = sqrt(u10.*u10 + v10.*v10); % wind_speed is the module of the two components u10 and v10
 
-r2cFile(ws,Date,OutPath,'wind',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
+util_r2cFile(ws,Date,OutPath,'wind',xOrigin,yOrigin,xCount,yCount,xDelta,yDelta,Reducc)
